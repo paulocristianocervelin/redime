0f9213558632d99ce158d83bd22c362137cc0604
paulocristianocervelin <66270219+paulocristianocervelin@users.noreply.github.com>
2025-11-13 22:44:52 -0300

    deploy 8

--- src/app/api/donations/route.ts
+++ src/app/api/donations/route.ts
@@ -1,6 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { PrismaClient } from '@prisma/client';
-import { getServerSession } from 'next-auth';
 
 const prisma = new PrismaClient();
 
